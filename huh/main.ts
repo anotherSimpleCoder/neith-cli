6d78fa752e74678449cf76f7f5b0c81cf4845052
Amsakan Bavan <amsakan.bavan@softwareag.com>
2024-10-08 11:56:39 +0200

    Merged rust rewrite into main

--- huh/main.ts
+++
@@ -1,6 +0,0 @@
-import { NeithServer } from 'https://github.com/anotherSimpleCoder/neith/raw/main/mod.ts';
-import { routes } from './routes.ts';
-
-NeithServer
-	.fromRoutes(routes)
-	.serve(3000);
--- huh/routes.ts
+++
@@ -1,3 +0,0 @@
-import { Route } from 'https://github.com/anotherSimpleCoder/neith/raw/main/mod.ts';
-
-export const routes: Route[] = [];
